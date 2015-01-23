31f1f959ee49fd5ff053b77b1b083659f7b9b156
john <john@john-Inspiron-N5050>
2015-01-23 11:19:13 -0500

    Setting up menu

--- glslTextEditor/src/glsltexteditor/GlslTextEditor.java
+++ glslTextEditor/src/glsltexteditor/GlslTextEditor.java
@@ -1,6 +1,9 @@
 package glsltexteditor;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 
 public class GlslTextEditor {
@@ -8,6 +11,7 @@ public class GlslTextEditor {
     public static void main(String[] args) {
         JFrame frame = createFrame();
         createJTA(frame);
+        createMenuBar(frame);
         frame.setVisible(true);
     }
     
@@ -23,6 +27,18 @@ public class GlslTextEditor {
         frame.add(textArea);
         textArea.setVisible(true);
         return textArea;
+    }
+    
+    public static JMenuBar createMenuBar(JFrame frame){
+        JMenuBar menuBar = new JMenuBar();
+        JMenu barItem = new JMenu("File");
+        JMenuItem menuItem = new JMenuItem("A menu item!");
+        barItem.add(menuItem);
+        menuBar.setSize(frame.getWidth(), frame.getHeight()/12);
+        frame.setJMenuBar(menuBar);
+        
+        menuBar.add(barItem);
+        return menuBar;
     }
     
 }
